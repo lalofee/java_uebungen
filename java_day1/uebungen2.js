8c49beedd0a8df182a91471abd8c337206e374f8
Angela Armstorfer <armstorfer@silverserver.at>
2017-11-29 12:02:08 +0100

    erste java uebungen

---
+++ java_day1/uebungen2.js
@@ -0,0 +1,40 @@
+var players = ["martin", "thomas", "peter"]
+
+document.write("The most valuable player of the match is " + (players[2]));
+
+document.write(`<br>`)
+document.write(`<br>`)
+/*Write a JavaScript program that will sort alphabetically the following words: 
+Tesla, BWM, Renault, Volvo, Mazda, Fiat and Ferari*/
+var cars =["Tesla", "BMW", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"]
+
+cars.sort();
+
+document.write(cars)
+
+document.write(`<br>`)
+document.write(`<br>`)
+/*Create a program that using push and pop methods will provide the following output:
+Start = apple, banana, kiwi,
+After Push = apple, banana, kiwi, orange
+After Pop = apple, banana, kiwi
+Each statement should be outputted on separate line.*/
+
+
+
+var fruits = ["apple", "banana", "kiwi"]
+
+fruits.push("orange");
+fruits.pop();
+
+document.write(fruits);
+
+document.write(`<br>`)
+document.write(`<br>`)
+/*Create a program that will output each fruit on a separate line in 
+the web browser from the following string "mango/cherries/kiwi/grapes/
+pear/peach/orange/lemon".*/
+
+var fruechte = ["mango", "cherries", "kiwi", "grapes", "pear"]
+
+document.write(fruechte/n)
